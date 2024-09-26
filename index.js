920266cd8fc3ec4369fd49a05537019e30300bb2
news-s <news-s>
2024-09-26 10:23:38 +0200

    ngfjkjgnc

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function closeWin(){
-    window.close();
+    document.write("<button onclick='window.close()'>Zamknij okno</button>")
 }
 
 function WinOpen(name, numer, height=380) 
 {
    window.open(name + ".html", "okienko_z" + numer, "toolbar=no,directories=no,menubar=no,height=380,width=160,top=200,left=400");
 }
+
+closeWin()
