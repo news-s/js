54320c7587ce8ecec9fa00051391d48d06408f51
news-s <news-s>
2024-10-03 11:18:31 +0200

    dsfdsfds

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ function WinOpen(name, numer, height=380)
 {
    window.open(name + ".html", "okienko_z" + numer, "toolbar=no,directories=no,menubar=no,height=380,width=160,top=200,left=400");
 }
-
-closeWin()
