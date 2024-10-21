ecada37ff535b8add2828af3965fde21ec69ef06
News-s <84093019+news-s@users.noreply.github.com>
2024-10-21 19:52:36 +0200

    holy shit

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function closeWin(){
-    document.write("<button onclick='window.close()'>Zamknij okno</button>")
-}
-
-function WinOpen(name, numer, height=380) 
-{
-   window.open(name + ".html", "okienko_z" + numer, "toolbar=no,directories=no,menubar=no,height=380,width=160,top=200,left=400");
-}
